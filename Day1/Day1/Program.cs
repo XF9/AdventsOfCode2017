1f3c00b2385bf5bbcb8735dbd276f7911e4bebbf
Michael Möhler <m.moehler@xf9.de>
2017-12-01 14:00:07 +0100

    Day1

---
+++ Day1/Day1/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Day1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var sum = 0;
+            var input = System.IO.File.ReadAllText(@"input.txt");
+
+            for (var index = 0; index < input.Length; index++)
+                if(input[index] == input[(index + 1) % input.Length])
+                    sum += Int32.Parse(input[index].ToString());
+            Console.WriteLine(sum);
+
+            sum = 0;
+
+            for (var index = 0; index < input.Length; index++)
+                if (input[index] == input[(index + input.Length/2) % input.Length])
+                    sum += Int32.Parse(input[index].ToString());
+            Console.WriteLine(sum);
+
+            Console.ReadKey();
+        }
+    }
+}
