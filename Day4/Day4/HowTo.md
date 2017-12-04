bb8df892350fee52450072c136c6bc7699955c90
Michael Möhler <m.moehler@xf9.de>
2017-12-04 19:46:22 +0100

    Day4

---
+++ Day4/Day4/HowTo.md
@@ -0,0 +1,5 @@
+﻿# Teil 1
+Distinct() liefert eine Liste ohne Duplikate. Ich muss dann nur die Anzahl der Elemente in der Liste mit der Anzahl der Elmenete ohne Duplikate vergleichen.
+
+# Teil 2
+Hier sortiere ich die Buchstaben in jedem Wort einfach nach dem Alphabet und verfahre dann genau so wie in Teil 1.
---
+++ Day4/Day4/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var valid = 0;
+            var input = System.IO.File.ReadLines(@"input.txt").ToList();
+
+            foreach (var line in input)
+            {
+                var words = line.Split(new Char[] {' '}, StringSplitOptions.RemoveEmptyEntries).ToList();
+                if (words.Count == words.Distinct().Count())
+                    valid++;
+            }
+
+            Console.WriteLine(valid);
+
+            valid = 0;
+            foreach (var line in input)
+            {
+                var words = line.Split(new Char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries).ToList();
+                var orderedWords = new List<String>();
+                foreach(var word in words)
+                    orderedWords.Add(String.Concat(word.OrderBy(c => c)));
+
+                if (orderedWords.Count == orderedWords.Distinct().Count())
+                    valid++;
+            }
+
+            Console.WriteLine(valid);
+            Console.ReadKey();
+        }
+    }
+}
