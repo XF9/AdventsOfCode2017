32cf4321da2ca491e8b4d4a93a81edb297e7b69f
Michael Möhler <m.moehler@xf9.de>
2017-12-09 17:52:59 +0100

    Day9

---
+++ Day9/Day9/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+
+namespace Day9
+{
+    class Program
+    {
+        public static (int Score, int GarbageLength) Solve(string group)
+        {
+            var depth = 1;
+            var score = 0;
+            var readingGarbage = false;
+            var garbageLength = 0;
+
+            for (int index = 0; index < group.Length; index++)
+            {
+                switch (group[index])
+                {
+                    case '!':
+                        index++;
+                        break;
+                    case '{':
+                        if (!readingGarbage)
+                            score += depth++;
+                        else
+                            garbageLength++;
+                        break;
+                    case '}':
+                        if(!readingGarbage)
+                            depth--;
+                        else
+                            garbageLength++;
+                        break;
+                    case '<':
+                        if(!readingGarbage)
+                            readingGarbage = true;
+                        else
+                            garbageLength++;
+                        break;
+                    case '>':
+                        readingGarbage = false;
+                        break;
+                    default:
+                        if (readingGarbage)
+                            garbageLength++;
+                        break;
+                }
+            }
+
+            return (score, garbageLength);
+        }
+
+        static void Main(string[] args)
+        {
+            var input = System.IO.File.ReadAllText("input.txt");
+            var result = Solve(input);
+            Console.WriteLine(result.Score);
+            Console.WriteLine(result.GarbageLength);
+            Console.ReadKey();
+        }
+    }
+}
