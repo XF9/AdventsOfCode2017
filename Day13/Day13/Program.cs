8a6ee43dffd8582a9a47960d7014d03ac602a325
Michael Möhler <m.moehler@xf9.de>
2017-12-13 06:49:33 +0100

    Day13

---
+++ Day13/Day13/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Day13
+{
+    class Program
+    {
+        public struct Layer
+        {
+            public int Depth { get; set; }
+            public int Range;
+        }
+
+        public static Layer ParseLine(string line)
+        {
+            var seperator = line.IndexOf(":");
+            return new Layer
+            {
+                Depth = Int32.Parse(line.Substring(0, seperator)),
+                Range = Int32.Parse(line.Substring(seperator + 2))
+            };
+        }
+
+        public static (int severity, bool caught) CalculateSeverity(IEnumerable<Layer> layers, int delay = 0)
+        {
+            var severity = 0;
+            bool caught = false;
+            foreach (var layer in layers)
+            {
+                if ((delay + layer.Depth) % ((layer.Range - 1) * 2) == 0)
+                {
+                    severity += layer.Range * layer.Depth;
+                    caught = true;
+                }
+            }
+            return (severity, caught);
+        }
+
+        static void Main(string[] args)
+        {
+            var input = System.IO.File.ReadAllLines("input.txt").Select(ParseLine);
+
+            Console.WriteLine(CalculateSeverity(input).severity);
+
+            var delay = 0;
+            while (CalculateSeverity(input, delay).caught)
+                delay++;
+
+            Console.WriteLine(delay);
+
+            Console.ReadKey();
+        }
+    }
+}
