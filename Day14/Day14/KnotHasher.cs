fee331122ca42d59eda2b7462f8b3d56e7a7e9df
Michael Möhler <m.moehler@xf9.de>
2017-12-14 20:53:54 +0100

    Day14

---
+++ Day14/Day14/KnotHasher.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Day14
+{
+    class KnotHasher
+    {
+        private IEnumerable<int> ParseListASCII(string text)
+        {
+            var result = new List<int>();
+            foreach (var bytevalue in Encoding.ASCII.GetBytes(text))
+                result.Add(bytevalue);
+
+            result.AddRange(new List<int> { 17, 31, 73, 47, 23 });
+            return result;
+        }
+
+        public static IEnumerable<int> GetValueList()
+        {
+            for (int value = 0; value < 256; value++)
+                yield return value;
+        }
+
+        public static void ShiftList(List<int> list, int offset)
+        {
+            for (int i = 0; i < offset; i++)
+            {
+                list.Add(list.First());
+                list.RemoveAt(0);
+            }
+        }
+
+        public static void UnshiftList(List<int> list, int shiftCount)
+        {
+            var shiftsLeft = list.Count - (shiftCount % list.Count);
+            ShiftList(list, shiftsLeft);
+        }
+
+        public static (int ShiftCount, int Skip) CalculateHash(List<int> values, List<int> lengths, int shiftCount = 0, int skip = 0)
+        {
+            foreach (var length in lengths)
+            {
+                var replacement = values.GetRange(0, length);
+                replacement.Reverse();
+
+                for (int index = 0; index < replacement.Count; index++)
+                    values[index] = replacement[index];
+
+                ShiftList(values, length + skip);
+                shiftCount += length + skip;
+                skip++;
+            }
+
+            return (shiftCount, skip);
+        }
+
+        public static IEnumerable<int> Compress(IEnumerable<int> list)
+        {
+            var copy = new List<int>(list);
+            while (copy.Count() >= 16)
+            {
+                int groupValue = 0;
+                foreach (int value in copy.Take(16))
+                    groupValue ^= value;
+
+                copy.RemoveRange(0, 16);
+                yield return groupValue;
+            }
+        }
+
+        public static string ToHex(IEnumerable<int> list)
+        {
+            var result = String.Empty;
+            foreach (var item in list)
+            {
+                var hex = item.ToString("X");
+                result += hex.Length == 1 ? "0" + hex : hex;
+            }
+            return result;
+        }
+
+        public string Hash(string input)
+        {
+            var values = GetValueList().ToList();
+            var lengths = ParseListASCII(input).ToList();
+
+            var skip = 0;
+            var shiftCount = 0;
+            for (int i = 0; i < 64; i++)
+            {
+                var result = CalculateHash(values, lengths, shiftCount, skip);
+                shiftCount = result.ShiftCount;
+                skip = result.Skip;
+            }
+
+            UnshiftList(values, shiftCount);
+            var compressed = Compress(values).ToList();
+            return ToHex(compressed);
+        }
+    }
+}
---
+++ Day14/Day14/Program.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+
+namespace Day14
+{
+    class Program
+    {
+        public const int FieldSize = 128;
+        public static string HexvalueToBits(string hex)
+        {
+            var value = Convert.ToInt32(hex, 16);
+            var bits = Convert.ToString(value, 2).PadLeft(4, '0');
+            return bits;
+        }
+
+        public static void FormGroup(int groupCount, Point coords, String[] occupiedPositions, Int32[,] groupMap)
+        {
+            var positionsToCheck = new List<Point> { coords };
+            while (positionsToCheck.Any())
+            {
+                var position = positionsToCheck.First();
+                positionsToCheck.Remove(position);
+                groupMap[position.Y, position.X] = groupCount;
+
+                var top = new Point(position.X, position.Y - 1);
+                if(NeedsToBeGrouped(top, occupiedPositions, groupMap) && !positionsToCheck.Contains(top))
+                    positionsToCheck.Add(top);
+
+                var bottom = new Point(position.X, position.Y + 1);
+                if (NeedsToBeGrouped(bottom, occupiedPositions, groupMap) && !positionsToCheck.Contains(bottom))
+                    positionsToCheck.Add(bottom);
+
+                var left = new Point(position.X - 1, position.Y);
+                if (NeedsToBeGrouped(left, occupiedPositions, groupMap) && !positionsToCheck.Contains(left))
+                    positionsToCheck.Add(left);
+
+                var right = new Point(position.X + 1, position.Y);
+                if (NeedsToBeGrouped(right, occupiedPositions, groupMap) && !positionsToCheck.Contains(right))
+                    positionsToCheck.Add(right);
+            }
+        }
+
+        public static bool NeedsToBeGrouped(Point point, String[] occupiedPositions, Int32[,] groupMap)
+        {
+            var inBounds = point.Y >= 0 && point.Y < FieldSize && point.X >= 0 && point.X < FieldSize;
+            var needsToBeGrouped = inBounds && occupiedPositions[point.Y][point.X] == '1' && groupMap[point.Y, point.X] == 0;
+            return needsToBeGrouped;
+        }
+
+        static void Main(string[] args)
+        {
+            var input = "ffayrhll";
+            var hasher = new KnotHasher();
+            var lines = new String[FieldSize];
+
+            for (int row = 0; row < FieldSize; row++)
+            {
+                var hashInput = $"{input}-{row}";
+                var hash = hasher.Hash(hashInput);
+
+                var bitstring = String.Empty;
+                foreach (var c in hash)
+                    bitstring += HexvalueToBits(c.ToString());
+
+                lines[row] = bitstring;
+            }
+
+            var filled = lines.SelectMany(l => l).Count(c => c == '1');
+            Console.WriteLine(filled);
+
+            var groupMap = new Int32[FieldSize, FieldSize];
+            var groupCount = 0;
+            for (int row = 0; row < FieldSize; row++)
+                for (int column = 0; column < FieldSize; column++)
+                    if (NeedsToBeGrouped(new Point(column, row), lines, groupMap))
+                        FormGroup(++groupCount, new Point(column, row), lines, groupMap);
+
+            Console.WriteLine(groupCount);
+            Console.ReadKey();
+        }
+    }
+}
